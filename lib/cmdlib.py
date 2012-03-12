cfaacefdcc368a752d74627e97a152e4ce1e814c
Constantinos Venetsanopoulos <cven@grnet.gr>
2012-03-12 17:49:18 +0200

    Implement the External Storage Interface
    With this commit we introduce the External Storage Interface
    to Ganeti, abbreviated: ExtStorage Interface.
    
    The ExtStorage Interface provides Ganeti with the ability to interact
    with externally connected shared storage pools, visible by all
    VM-capable nodes. This means that Ganeti is able to handle VM disks
    that reside inside a NAS/SAN or any distributed block storage provider.
    
    The ExtStorage Interface provides a clear API, heavily inspired by the
    gnt-os-interface API, that can be used by storage vendors or sysadmins
    to write simple ExtStorage Providers (correlated to gnt-os-interface's
    OS Definitions). Those Providers will glue externally attached shared
    storage with Ganeti, without the need of preprovisioned block devices
    on Ganeti VM-capable nodes as confined be the current `blockdev' disk
    template.
    
    To do so, we implement a new disk template called `ext' (of type
    DTS_EXT_MIRROR) that passes control to externally provided scripts
    (the ExtStorage Provider) for the template's basic functions:
    
     create / attach / detach / remove / grow
    
    The scripts reside under ES_SEARCH_PATH (correlated to OS_SEARCH_PATH)
    and only one ExtStorage Provider is supported called `ext'.
    
    The disk's logical id is the tuple ('ext', UUID.ext.diskX), where UUID
    is generated as in disk template `plain' and X is the disk's index.
    
    Signed-off-by: Constantinos Venetsanopoulos <cven@grnet.gr>

--- lib/cmdlib.py
+++ lib/cmdlib.py
@@ -8511,9 +8511,9 @@ class TLMigrateInstance(Tasklet):
       self._GoReconnect(False)
       self._WaitUntilSync()
 
-    # If the instance's disk template is `rbd' and there was a successful
-    # migration, unmap the device from the source node.
-    if self.instance.disk_template == constants.DT_RBD:
+    # If the instance's disk template is `rbd' or `ext' and there was a
+    # successful migration, unmap the device from the source node.
+    if self.instance.disk_template in (constants.DT_RBD, constants.DT_EXT):
       disks = _ExpandCheckDisks(instance, instance.disks)
       self.feedback_fn("* unmapping instance's disks from %s" % source_node)
       for disk in disks:
@@ -8762,6 +8762,7 @@ def _GenerateDRBD8Branch(lu, primary, secondary, size, vgnames, names,
 _DISK_TEMPLATE_NAME_PREFIX = {
   constants.DT_PLAIN: "",
   constants.DT_RBD: ".rbd",
+  constants.DT_EXT: ".ext",
   }
 
 
@@ -8771,6 +8772,7 @@ _DISK_TEMPLATE_DEVICE_TYPE = {
   constants.DT_SHARED_FILE: constants.LD_FILE,
   constants.DT_BLOCK: constants.LD_BLOCKDEV,
   constants.DT_RBD: constants.LD_RBD,
+  constants.DT_EXT: constants.LD_EXT,
   }
 
 
@@ -8849,6 +8851,8 @@ def _GenerateDiskTemplate(lu, template_name, instance_name, primary_node,
                                        disk[constants.IDISK_ADOPT])
     elif template_name == constants.DT_RBD:
       logical_id_fn = lambda idx, _, disk: ("rbd", names[idx])
+    elif template_name == constants.DT_EXT:
+      logical_id_fn = lambda idx, _, disk: ("ext", names[idx])
     else:
       raise errors.ProgrammerError("Unknown disk template '%s'" % template_name)
 
@@ -9113,6 +9117,7 @@ def _ComputeDiskSize(disk_template, disks):
     constants.DT_SHARED_FILE: 0,
     constants.DT_BLOCK: 0,
     constants.DT_RBD: 0,
+    constants.DT_EXT: 0,
   }
 
   if disk_template not in req_size_dict:
@@ -9953,6 +9958,9 @@ class LUInstanceCreate(LogicalUnit):
         # Any function that checks prerequisites can be placed here.
         # Check if there is enough space on the RADOS cluster.
         _CheckRADOSFreeSpace()
+      elif self.op.disk_template == constants.DT_EXT:
+        # FIXME: Function that checks prereqs if needed
+        pass
       else:
         # Check lv size requirements, if not adopting
         req_sizes = _ComputeDiskSizePerVG(self.op.disk_template, self.disks)
@@ -11665,7 +11673,8 @@ class LUInstanceGrowDisk(LogicalUnit):
 
     if instance.disk_template not in (constants.DT_FILE,
                                       constants.DT_SHARED_FILE,
-                                      constants.DT_RBD):
+                                      constants.DT_RBD,
+                                      constants.DT_EXT):
       # TODO: check the free disk space for file, when that feature will be
       # supported
       _CheckNodesFreeDiskPerVG(self, nodenames,

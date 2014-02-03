c210ef6aa83007b11e98f050c86261d64afa7be7
Klaus Aehlig <aehlig@google.com>
2014-02-03 10:54:27 +0100

    (2.11) Add function providing the canonical andRestArguments
    The field catching the remaining fields will always be of the same
    shape, so add a function for this to make usage simple.
    
    Signed-off-by: Klaus Aehlig <aehlig@google.com>
    Reviewed-by: Petr Pudlak <pudlak@google.com>
    Signed-off-by: Dimitris Aragiorgis <dimara@grnet.gr>

--- src/Ganeti/THH.hs
+++ src/Ganeti/THH.hs
@@ -47,6 +47,7 @@ module Ganeti.THH ( declareSADT
                   , genLuxiOp
                   , Field (..)
                   , simpleField
+                  , andRestArguments
                   , withDoc
                   , defaultField
                   , optionalField
@@ -120,6 +121,20 @@ simpleField fname ftype =
         , fieldDefault     = Nothing
         , fieldConstr      = Nothing
         , fieldIsOptional  = NotOptional
+        , fieldDoc         = ""
+        }
+
+-- | Generate an AndRestArguments catch-all field.
+andRestArguments :: String -> Field
+andRestArguments fname =
+  Field { fieldName        = fname
+        , fieldType        = [t| M.Map String JSON.JSValue |]
+        , fieldRead        = Nothing
+        , fieldShow        = Nothing
+        , fieldExtraKeys   = []
+        , fieldDefault     = Nothing
+        , fieldConstr      = Nothing
+        , fieldIsOptional  = AndRestArguments
         , fieldDoc         = ""
         }
 

psomas/ganeti

commit bd770b4bef940c31007e2d29e2963da2f65c9a27

ifdown: Extract env creation from ConfigureNic()
Introduce new helper method that creates the environment passed
to ifup/ifdown scripts. Instance tags and NIC index will be passed
only during ifup script since only then are available and valid.
Both can change until a NIC gets removed and thus will not be passed
to ifdown script.

Note that TAP name may be not available during ifdown script for
old instances without UUID indexed NIC files. That's why it gets
included in env only if it exists.

Signed-off-by: Dimitris Aragiorgis <dimara@grnet.gr>

lib/hypervisor/hv_base.py
@@ -168,30 +168,21 @@ def ParamInSet(required, my_set):
   return (required, fn, err, None, None)
 
 
-def ConfigureNIC(cmd, instance, seq, nic, tap):
-  """Run the network configuration script for a specified NIC
+def CreateNICEnv(instance_name, nic, tap, seq=None, instance_tags=None):
+  """Create environment variables for a specific NIC
 
-  @type cmd: string
-  @param cmd: command to run
-  @type instance: instance object
-  @param instance: instance we're acting on
-  @type seq: int
-  @param seq: nic sequence number
-  @type nic: nic object
-  @param nic: nic we're acting on
-  @type tap: str
-  @param tap: the host's tap interface this NIC corresponds to
+  This is needed during NIC ifup/ifdown scripts.
+  Since instance tags may change during NIC creation and removal
+  and because during cleanup the instance object is not available, we
+  pass them only upon NIC creation (instance startup/NIC hot-plugging).
 
   """
   env = {
     "PATH": "%s:/sbin:/usr/sbin" % os.environ["PATH"],
-    "INSTANCE": instance.name,
+    "INSTANCE": instance_name,
     "MAC": nic.mac,
     "MODE": nic.nicparams[constants.NIC_MODE],
-    "INTERFACE": tap,
-    "INTERFACE_INDEX": str(seq),
     "INTERFACE_UUID": nic.uuid,
-    "TAGS": " ".join(instance.GetTags()),
   }
 
   if nic.ip:
@@ -210,9 +201,39 @@ def ConfigureNIC(cmd, instance, seq, nic, tap):
     n = objects.Network.FromDict(nic.netinfo)
     env.update(n.HooksDict())
 
+  if instance_tags:
+    env["TAGS"] = " ".join(instance_tags)
+
+  # This should always be available except for old instances in the
+  # cluster without uuid indexed tap files.
+  if tap:
+    env["INTERFACE"] = tap
+
+  if seq:
+    env["INTERFACE_INDEX"] = str(seq)
+
   if nic.nicparams[constants.NIC_MODE] == constants.NIC_MODE_BRIDGED:
     env["BRIDGE"] = nic.nicparams[constants.NIC_LINK]
 
+  return env
+
+
+def ConfigureNIC(cmd, instance, seq, nic, tap):
+  """Run the network configuration script for a specified NIC
+
+  @type cmd: string
+  @param cmd: command to run
+  @type instance: instance object
+  @param instance: instance we're acting on
+  @type seq: int
+  @param seq: nic sequence number
+  @type nic: nic object
+  @param nic: nic we're acting on
+  @type tap: str
+  @param tap: the host's tap interface this NIC corresponds to
+
+  """
+  env = CreateNICEnv(instance.name, nic, tap, seq, instance.GetTags())
   result = utils.RunCmd(cmd, env=env)
   if result.failed:
     raise errors.HypervisorError("Failed to configure interface %s: %s;"

commit 6070be5bb0f3810c44faf4bc4dc75c3d00496042

Fix NIC env in case seq is 0
Commit bd770b4 introduced a bug where the INTERFACE_INDEX was not
exported in scripts' environmenti if NIC's seq was 0. Fix this.

Signed-off-by: Dimitris Aragiorgis <dimara@grnet.gr>

lib/hypervisor/hv_base.py
@@ -209,7 +209,7 @@ def CreateNICEnv(instance_name, nic, tap, seq=None, instance_tags=None):
   if tap:
     env["INTERFACE"] = tap
 
-  if seq:
+  if seq is not None:
     env["INTERFACE_INDEX"] = str(seq)
 
   if nic.nicparams[constants.NIC_MODE] == constants.NIC_MODE_BRIDGED: